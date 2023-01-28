b5738c48d0486d116b8bf45d150efcd016d5dfae
anuj <anujkumarpandey2727@gmail.com>
2023-01-29 01:45:26 +0530

    added sum.cpp files

---
+++ sum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a;          // taking user input 
+    cin>>b;
+    cout<<a+b;         // printing the sum 
+    return 0;
+}
